41a2557ef5cd3f302dda7369e98cf1728ba41e73
Paula <paulamazoster@gmail.com>
2020-05-07 21:06:27 -0500

    Clase 11: Ejercicios prevent default

--- 11 - Events-async-arrays/03 - prevent-default/form.js
+++ 11 - Events-async-arrays/03 - prevent-default/form.js
@@ -1,1 +1,21 @@
-console.log('Hola mundo!');
+const homepage = document.querySelector('.homepage');
+const form = document.querySelector('[name="signup"]');
+
+homepage.addEventListener('click', function(event){
+    const shoulChange = confirm('Quieres comprar cositas?');
+    if (!shoulChange) {
+        event.preventDefault();
+    }
+});
+
+
+// Agreagmos un evento para que permita hacer submit del formulario
+// sólo si el nombre contiene 'paula' en el
+form.addEventListener('submit', function(event){
+    const name = event.currentTarget.name.value;
+    if (!name.includes('paula')) {
+        event.preventDefault();
+        alert('Sorry :(');
+    }
+});
+
