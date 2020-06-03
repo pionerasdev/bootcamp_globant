b75992b03b5d7cac2392abace840c6ba81c784ba
Paula <paulamazoster@gmail.com>
2020-06-02 22:31:53 -0500

    Clase 16: código base

---
+++ 16 - Intro React/base-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hola mundo react!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
